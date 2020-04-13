8a58e942aa66e83e3f4fc41c5702c5e196bcf1fa
alexSchwitthal <schwitthal.alexandre@gmail.com>
2020-04-14 01:23:33 +0200

    début TD04

---
+++ td04/exercice1/exercice1.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("canvas");
+canvas.width  = 1200;
+canvas.height = 600;
+var ctx = canvas.getContext('2d');
+
+
+function changeTo(element) {
+  alert(element);
+}
