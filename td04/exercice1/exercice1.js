120d560e505cbd2c7ba675a291768242cc09f7a1
alexSchwitthal <schwitthal.alexandre@gmail.com>
2020-04-22 15:23:11 +0200

    finalisation exercice 1

--- td04/exercice1/exercice1.js
+++ td04/exercice1/exercice1.js
@@ -1,41 +1,57 @@
+// initialisation du canvas
 let canvas = document.getElementById("canvas");
 canvas.width = 1200;
 canvas.height = 600;
 var ctx = canvas.getContext('2d');
+
+// initialisation des variables
+
+// correspond à la valeur de la marge du body, afin d'éviter un décalage entre les coordonnées du points et du canvas
 var offsetX = 20;
 var offsetY = 20;
-var selected = 'crayon';
-var actif = false;
-var taille = 12;
 
+var selected = 'crayon'; // element de dessin selectionné
+var taille = 12; // taille en px du dessin
+var actif = false; // actif est true si le clic souris est enfoncé, false sinon
+
+
+// coordonnées X,Y de départ, valeur changer à chaque clic de la souris sur le canvas
 var startX;
 var startY;
-var circleX;
-var circleY;
 
+// liste des points sauvegardés
 var storedPoints = [];
 var listPoints = [];
-var storedLines = [];
-var storedRects = [];
-var storedCircles = [];
-var listCircles = [];
 
+// liste des lignes sauvegardés
+var storedLines = [];
+
+// liste des rectangles sauvegardés
+var storedRects = [];
+
+// liste des cercles sauvegardés
+var storedCircles = [];
+
+
+// ajout des évenements sur le canvas (voir fonction ci-dessous)
 canvas.addEventListener('mousemove', onMouseMove, false);
 canvas.addEventListener('mousedown', onMouseDown, false);
 canvas.addEventListener('mouseup', onMouseUp, false);
 
-
-
+// quand la souris est enfoncé :
+// changement de la valeur d'actif et de startX, startY sur la position courante
 function onMouseDown(e) {
   actif = true;
   startX = parseInt(e.clientX - offsetX);
   startY = parseInt(e.clientY - offsetY);
 }
 
+// quand la souris est relaché :
+// ajoute l'element dessiné à la liste lui correspondant et actualise le canvas
 function onMouseUp(e) {
   actif = false;
-  var x = parseInt(e.clientX - offsetX);
-  var y = parseInt(e.clientY - offsetY);
+  let x = parseInt(e.clientX - offsetX);
+  let y = parseInt(e.clientY - offsetY);
   switch(selected) {
     case 'crayon':
       storedPoints.push({
@@ -81,22 +97,12 @@ function onMouseUp(e) {
   draw();
 }
 
+// quand la souris est bougé :
+// selon l'outils de dessin selectionné, fait un dessin dynamique qui ne sera sauvegardé
+// qu'au relachement de la souris (voir onMouseUp)
 function onMouseMove(e) {
   var x = parseInt(e.clientX - offsetX);
   var y = parseInt(e.clientY - offsetY);
-
-  // Get the mouse position.
-  if (e.layerX >= 0) {
-    // Firefox
-    x = e.layerX;
-    y = e.layerY;
-  }
-  else if (e.offsetX >= 0) {
-    // Opera
-    x = e.offsetX;
-    y = e.offsetY;
-  }
-
 
   if(!actif) {
     ctx.beginPath();
@@ -151,30 +157,17 @@ function onMouseMove(e) {
         ctx.lineWidth = taille;
         ctx.strokeStyle = "black";
         ctx.stroke();
-        // ctx.beginPath();
-        // ctx.arc(startX, startY,
-        //   Math.sqrt(Math.abs(x - startX) + Math.abs(y - startY))
-        // * Math.sqrt(Math.abs(x - startX) + Math.abs(y - startY)),
-        // 0, 2 * Math.PI);
-        // ctx.lineWidth = taille;
-        // ctx.strokeStyle = "black";
-        // ctx.stroke();
         break;
     }
   }
 }
 
+// fonction permettant l'actualisation du dessin, le but est ainsi de redéssiné tout ce qui n'est pas temporaire
 function draw() {
+  // réinitialise le canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < storedLines.length; i++) {
-    ctx.beginPath();
-    ctx.moveTo(storedLines[i].x1, storedLines[i].y1);
-    ctx.lineTo(storedLines[i].x2, storedLines[i].y2);
-    ctx.lineWidth = storedLines[i].taille;
-    ctx.strokeStyle = "black";
-    ctx.stroke();
-  }
 
+  // redessine la totalité des points (outils crayon)
   for (var i = 0; i < storedPoints.length; i++) {
     ctx.beginPath();
     ctx.moveTo(storedPoints[i].startX, storedPoints[i].startY);
@@ -187,6 +180,17 @@ function draw() {
     ctx.stroke();
   }
 
+  // redessine la totalité des lignes
+  for (var i = 0; i < storedLines.length; i++) {
+    ctx.beginPath();
+    ctx.moveTo(storedLines[i].x1, storedLines[i].y1);
+    ctx.lineTo(storedLines[i].x2, storedLines[i].y2);
+    ctx.lineWidth = storedLines[i].taille;
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+  }
+
+  // redessine la totalité des rectaangles
   for (var i = 0; i < storedRects.length; i++) {
     ctx.beginPath();
     ctx.rect(storedRects[i].leftX, storedRects[i].topY,
@@ -196,15 +200,8 @@ function draw() {
     ctx.stroke();
   }
 
+  // redessine la totalité des cercles
   for (var i = 0; i < storedCircles.length; i++) {
-    //ctx.beginPath();
-    // ctx.arc(storedCircles[i].startX, storedCircles[i].startY,
-    //   Math.sqrt(Math.abs(storedCircles[i].x - storedCircles[i].startX) + Math.abs(storedCircles[i].y - storedCircles[i].startY))
-    // * Math.sqrt(Math.abs(storedCircles[i].x - storedCircles[i].startX) + Math.abs(storedCircles[i].y - storedCircles[i].startY)),
-    // 0, 2 * Math.PI);
-    // ctx.lineWidth = storedCircles[i].taille;
-    // ctx.strokeStyle = "black";
-    // ctx.stroke();
     ctx.beginPath();
     for (var j = 0 * Math.PI; j < 2.01 * Math.PI; j += 0.01 ) {
         let circleX = storedCircles[i].startX -
@@ -226,14 +223,6 @@ function draw() {
             ctx.lineTo(circleX, circleY);
         }
     }
-    // for(var j = 0; j < storedCircles[i].listCircles.length; j++) {
-    //   if(j == 0) {
-    //     ctx.moveTo(storedCircles[i].listCircles[j].x, storedCircles[i].listCircles[j].y);
-    //   }
-    //   else {
-    //     ctx.lineTo(storedCircles[i].listCircles[j].x, storedCircles[i].listCircles[j].y);
-    //   }
-    // }
 
     ctx.lineWidth = taille;
     ctx.strokeStyle = "black";
@@ -241,7 +230,10 @@ function draw() {
   }
 }
 
-// -- function secondaire -- //
+// -- fonctions secondaire -- //
+
+// réinitialise totalement le canvas, en supprimant le contenu de chacune des listes
+// appelé via le bouton de réinitialisation
 function reset() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   storedLines.length = 0;
@@ -250,12 +242,15 @@ function reset() {
   storedCircles.length = 0;
 }
 
+// change la taille du crayon
+// appelé via la barre de changement de taille du crayon
 function changeSize(size) {
   taille = size;
-  var affichage = document.getElementById("affichage");
-  affichage.innerHTML = size;
+  document.getElementById("affichage").innerHTML = size;
 }
 
+// change l'élément de dessin selectionné
+// appelé au clic de chacun des boutons (sauf le bouton de réinitilisation)
 function changeTo(element) {
   const buttons = document.querySelectorAll('button');
   if (element != 'reset') {
@@ -273,6 +268,7 @@ function changeTo(element) {
   }
 }
 
+// fonction retournant un rectangle
 function getRect(startX, startY, currentX, currentY) {
   let rect = new Object();
   if (startX < currentX) {
